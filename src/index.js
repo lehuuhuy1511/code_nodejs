0de9a3590b6ffeb99b5142fa67f6bf847d065837
lehuuhuy1511 <146902635+lehuuhuy1511@users.noreply.github.com>
2024-10-29 10:37:43 +0700

    giaodien

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+const part = reqiure('part')
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars');
 const app = express()
 const port = 3000
 
-app.engine('hanslebars',handlebars())
-app.set('view engine ','hamslebars ')
+app.use(express.static(path.join(__dirname,'public')));
+
+app.engine('hbs',handlebars({
+    extname:'.hbs'
+}));
+app.set('view engine ','hbs ');
+app.set('view',path.join(__driname,'views'));
+
+console.log('PATH:', path.join(__dirname, 'resources/views'))
 
 app.use(morgan('combined'))
 
@@ -13,5 +21,9 @@ app.get ('/' , ( req ,res)=> {
 
 res.sendr('home')
 })
+app.get ('/news' , ( req ,res)=> {
+
+    res.sendr('news')
+    })
 
 app.listen (port,()=> console.log(`Example app listening at http://localhost:${port}`))
